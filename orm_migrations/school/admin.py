cea3a12402c1fda78c973afaf636142e39427677
Дмитрий Никифоров <nikiforov_d.1973@mail.ru>
2022-09-13 16:37:01 +0400

    migrate_NEW

--- orm_migrations/school/admin.py
+++ orm_migrations/school/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Student, Teacher
+
+
+class MemberclassInline(admin.TabularInline):
+    model = Student.teacher.through
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    inlines = [MemberclassInline]
+    include = ['student']
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    inlines = [MemberclassInline]
+
 # Register your models here.
